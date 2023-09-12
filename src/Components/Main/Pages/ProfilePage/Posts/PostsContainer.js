22ab6a9e05afbc80b279959c79fef1abb2c782bf
Your Name <you@example.com>
2023-09-12 11:07:54 +0300

    feat: logic for addPost/addMessage (redux + useSelector/useDispatch/useCallback)

--- src/Components/Main/Pages/ProfilePage/Posts/PostsContainer.js
+++ src/Components/Main/Pages/ProfilePage/Posts/PostsContainer.js
@@ -1,13 +1,22 @@
+import { addPost, setPost } from '../../../../../redux/actions/profile';
 import { Posts } from './Posts';
-import { useSelector } from 'react-redux';
-import { useActions } from '../../../../../utils/hooks/useActions';
+import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
 
 export const PostsContainer = () => {
   const posts = useSelector((state) => state.profile.posts);
   const postText = useSelector((state) => state.profile.postText);
-  const {setPost, addPost} = useActions();
+  const dispatch = useDispatch();
+  const setPostCallback = useCallback(
+    (text) => dispatch(setPost(text)),
+    [dispatch]
+  );
+  const addPostCallback = useCallback(
+    (text) => dispatch(addPost(text)),
+    [dispatch]
+  );
 
   return (
-    <Posts posts={posts} postText={postText} setPost={setPost} addPost={addPost} />
+    <Posts posts={posts} postText={postText} setPost={setPostCallback} addPost={addPostCallback} />
   );
 };
--- src/redux/actions/actions.js
+++
@@ -1,7 +0,0 @@
-import * as messagesActionCreators from "./messages";
-import * as profileActionCreators from "./profile";
-
-export const actionCreators = {
-  ...messagesActionCreators,
-  ...profileActionCreators,
-};
