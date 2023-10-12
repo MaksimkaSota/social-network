ed10575ca2846e469ec1aaa0d37629b2147a5662
Your Name <you@example.com>
2023-10-12 14:14:57 +0300

    refactor: regenerated and hid the key

--- src/api/http.js
+++ src/api/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const URL = 'https://social-network.samuraijs.com/api/1.0/';
-const KEY = 'eeef07f4-5659-4447-a94e-52a1e5f5b9ba';
+const KEY = process.env.KEY;
 
 export const http = axios.create({
   withCredentials: true,
